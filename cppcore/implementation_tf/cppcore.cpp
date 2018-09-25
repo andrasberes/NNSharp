7e83ddea749b7ad3ee848196c935c4e7a88e7739
andrasberes <andrisberes@gmail.com>
2018-09-26 00:00:19 +0200

    first working test with TF_Tensor from tensorflow C API

--- cppcore/implementation_tf/cppcore.cpp
+++ cppcore/implementation_tf/cppcore.cpp
@@ -1,41 +1,67 @@
-// TryCNTKcpp.cpp : Defines the entry point for the console application.
-//
-
 #include <vector>
 #include <iostream>
-
-//region: TF MINI TEST
+#include <stdio.h>
+#include <algorithm>
 
 #define COMPILER_MSVC
-#include "tensorflow.h"
-#include "nnsharp.h"
+extern "C" {
+	#include "tensorflow.h"		//Tensorflow C API
+}
 
 void tf_test() {
 	
 	std::cout << "Hi!" << std::endl;
-
+	
 	const char* version = TF_Version();
 	std::cout << version[0] << version[1] << version[2] << version[3] << version[4] << std::endl;
-	TF_Buffer* ops = TF_GetAllOpList();
-	int d;
-	std::cin >> d;
+	//TF_Buffer* ops = TF_GetAllOpList();
+
+	//init
+	const std::vector<int64_t> dimensions = { 2, 2 };	//{depth, row, column}
+	std::size_t data_size = sizeof(int64_t);
+	for (auto i : dimensions) {
+		data_size *= i;
+	}
+	const std::vector<int64_t> data = {
+		1, 2,
+		3, 4};
+
+	TF_Tensor* myTensor = TF_AllocateTensor(TF_INT64, dimensions.data(), 
+		static_cast<int64_t>(dimensions.size()), data_size);
 	
+	if (myTensor != nullptr && TF_TensorData(myTensor) != nullptr) {
+		std::memcpy(TF_TensorData(myTensor), data.data(), std::min(data_size, TF_TensorByteSize(myTensor)));
+	}
+
+	//return values 
+	TF_DataType retDataType = TF_TensorType(myTensor);
+	int retDimension = TF_NumDims(myTensor);
+	const auto retDataBuffer = static_cast<int64_t*>(TF_TensorData(myTensor));
+
+	//check databuffer
+	for (std::size_t i = 0; i < data.size(); ++i) {
+		if (retDataBuffer[i] != data[i]) {
+			std::cout << "Element: " << retDataBuffer[i] << " does not match" << std::endl;
+		} else {
+			std::cout << "Element: " << retDataBuffer[i] << " matches. VERY GOOD!" << std::endl;
+		}
+	}
+
+	std::cout << "Dimension: " << retDimension << std::endl;
+	
+	//deinit
+	TF_DeleteTensor(myTensor);
 }
 
-// endregion (TF MINI TEST)
 
-int main(int argc, char *argv[])
-{
-
-	//CNTK_test();
-	//tf_test();
-	
-
-    return 0;
+int main(int argc, char *argv[]) {
+	tf_test();
+	std::getchar();
+	return 0;
 }
 
 // DLL test
-
+/*
 core::tensor::Tensor::Tensor(DataType data_type, int dim, int* shape) {
 
 	if (data_type == DataType::Integer) {
@@ -87,4 +113,4 @@ int core::tensor::tensor_integer_get(TensorInteger * tensor_in, int idx)
 }
 
 
-
+*/
